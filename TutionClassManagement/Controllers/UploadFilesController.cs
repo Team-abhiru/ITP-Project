0091b9069e33d7d178612515f51c2b5e8f9b9d14
Jayawardana JTH <tiran2323@gmail.com>
2020-03-09 08:41:56 +0530

    Update referances and create upload Section

---
+++ TutionClassManagement/Controllers/UploadFilesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace TutionClassManagement.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        // GET: UploadFiles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    String fileneme = Path.GetFileName(file.FileName);
+                    string filepath = Path.Combine(Server.MapPath("../Files_Upload"), fileneme);
+                    file.SaveAs(filepath);
+                }
+                ViewBag.Message = "Uploade file Saved successfully";
+                return View();
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Uploade file Not Saved successfully";
+                return View();
+
+            }
+
+        }
+    }
+}
